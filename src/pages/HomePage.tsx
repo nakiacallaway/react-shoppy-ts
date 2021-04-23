6a32cb584b5654239422bb89871f24c102ba0a0c
Nakia Callaway <nik.callaway@outlook.com>
2021-04-23 15:28:03 -0700

    feat(products api)added product types and imported into homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,38 @@
+import {useState, useEffect} from 'react';
+
 const HomePage = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts();
+    }, [] );
+
+    const fetchProducts = async (): Promise<void> => {
+        try {
+            let prods = await (await fetch('https://fakestoreapi.com/products')).json();
+            
+            setProducts(prods);
+        }
+
+        catch (e) {
+            console.log(e);
+        }
+    }
     return (
     <div id='home'>
         <div className="row text-center">
             <div className="col">
                 <h2>Home Page</h2>
             </div>
+        </div>
+        <div className="row">
+            <ul className="list-group">
+                {products.map((product, index) => {
+                    return (
+                        <li className="list-group-item" key={index}>{product.title}</li>
+                    )
+                })}
+            </ul>
         </div>
     </div>
     )
