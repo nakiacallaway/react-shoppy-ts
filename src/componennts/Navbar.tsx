c8c5a405d4c4079f4eb3e9cb9000d6fd71d655f2
Nakia Callaway <nik.callaway@outlook.com>
2021-04-23 14:59:06 -0700

    feat(navbar):added navbar to app and tested links

--- src/componennts/Navbar.tsx
+++ src/componennts/Navbar.tsx
@@ -4,14 +4,14 @@ const Navbar = () => {
     return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link to='/'>Shopsy</Link>
-
-        <div className="navbar-nav">
-            <Link className="nav-link" to='/'>Home</Link>
-            <Link className="nav-link" to='/cart'>Cart</Link>
-            <Link className="nav-link" to='/checkout'>Checkout</Link>
-            <Link className="nav-link" to='/products/1'>Product</Link>
+        <div className="collapse navbar-collapse">
+            <div className="navbar-nav">
+                <Link className="nav-link" to='/'>Home</Link>
+                {/* <Link className="nav-link" to='/cart'>Cart</Link>
+                <Link className="nav-link" to='/checkout'>Checkout</Link>
+                <Link className="nav-link" to='/products/1'>Product</Link> */}
+            </div>
         </div>
-
         <div className="sh-cart-icon">
             <div className="navbar-nav">
             <Link className="nav-link" to='/cart'>Cart</Link>
@@ -23,5 +23,4 @@ const Navbar = () => {
 }
 
 export default Navbar;
-
 
