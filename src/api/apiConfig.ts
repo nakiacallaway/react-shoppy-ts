ac2c8ef479f4a43648dd44619ff2d295859a0e72
Nakia Callaway <nik.callaway@outlook.com>
2021-05-03 13:40:54 -0700

    feat(homepage)styled cards to display products

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://fakestoreapi.com',
-    timeout: 1000,
     headers: {
         'Content-type': 'application/json',
     }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,7 @@
 import {useState, useEffect, useContext} from 'react';
+import {Link} from 'react-router-dom';
 import {GlobalContext} from '../context/GlobalContext';
+import '../App.css';
 
 const HomePage = () => {
     const {products, getProducts} = useContext(GlobalContext);
@@ -20,13 +22,29 @@ const HomePage = () => {
             
         </div>
         <div className="row">
-            <ul className="list-group">
-                {products.map((product, index) => {
+            
+                {products.map((product, i) => {
                     return (
-                        <li className="list-group-item" key={index}>{product.title}</li>
-                    )
+                        <div className="col-sm-12 col-md-3 mb-3" key={i}>
+                            <div className="card h-100">
+                                <div className="sh-card-img">
+                                    <div className="sh-bg-img" style={{backgroundImage: `url(${product.image})`}}>
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className='card-title'><Link to={`/products/${product.id}`}> {product.title}</Link> </h5>
+                                        <div className='d-flex justify-content-between align-items-center'>
+                                            <strong>${product.price}</strong>
+                                            <span className="badge-warning">{product.category}</span>
+                                        </div>
+                                    </div>    
+                                    <div className="card-footer">
+                                        <button className="btn btn-block btn-primary">Add to cart</button>
+                                    </div>        
+                                </div>
+                            </div>
+                        </div>)
                 })}
-            </ul>
+           
         </div>
     </div>
     )
